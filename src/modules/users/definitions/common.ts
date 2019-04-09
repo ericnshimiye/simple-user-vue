09e55e900f7e91c57a891403446f6dc8468ed6f9
Eric Nshimiye <enshimiye@dialoginsight.com>
2019-04-09 16:23:19 -0400

    add shared enums

---
+++ src/modules/users/definitions/common.ts
@@ -0,0 +1,69 @@
+export const GENDERS = [
+  {
+    value: "female",
+    label: "Female"
+  },
+  {
+    value: "male",
+    label: "Male"
+  },
+  {
+    value: "LGBTQQIP2SAA",
+    label: "LGBTQQIP2SAA"
+  }
+];
+
+export const PROVINCES = [
+  {
+    value: "AB",
+    label: "Alberta"
+  },
+  {
+    value: "BC",
+    label: "British Columbia"
+  },
+  {
+    value: "MB",
+    label: "Manitoba"
+  },
+  {
+    value: "NB",
+    label: "New Brunswick"
+  },
+  {
+    value: "NL",
+    label: "Newfoundland and Labrador"
+  },
+  {
+    value: "NT",
+    label: "Northwest Territories"
+  },
+  {
+    value: "NS",
+    label: "Nova Scotia"
+  },
+  {
+    value: "NU",
+    label: "Nunavut"
+  },
+  {
+    value: "ON",
+    label: "Ontario"
+  },
+  {
+    value: "PE",
+    label: "Prince Edward Island"
+  },
+  {
+    value: "QC",
+    label: "Quebec"
+  },
+  {
+    value: "SK",
+    label: "Saskatchewan"
+  },
+  {
+    value: "YT",
+    label: "Yukon"
+  }
+];
